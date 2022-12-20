f70c7ffd0360a30244e6ca20b5bebfceb8341fff
RahimLog <abderrahmane.sitayeb@gmail.com>
2022-12-20 14:53:36 +0100

    first commit login

---
+++ php/index.php
@@ -0,0 +1,30 @@
+<?php 
+session_start();
+    include("connection.php");
+    include("function.php");
+
+    $user_data = check_login($con);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Club Sportif</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+    <a href="login.php">Login</a>
+    <a href="signup.php">Signup</a>
+    <h1> This is the index page</h1>
+
+    <br>
+    Hello, Username.
+    
+</body>
+</html>
