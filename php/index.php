186605e6c8e8832a90d425b6fcf1d77242489bd7
RahimLog <abderrahmane.sitayeb@gmail.com>
2022-12-21 09:51:48 +0100

    index commit

--- php/index.php
+++ php/index.php
@@ -18,13 +18,28 @@ session_start();
     <title>Club Sportif</title>
 </head>
 <body>
+    <nav>
+        
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
     <a href="logout.php">Logout</a>
     <a href="login.php">Login</a>
     <a href="signup.php">Signup</a>
     <h1> This is the index page</h1>
 
     <br>
-    Hello, Username.
+    Hello, <?php echo $user_data['user_name']; 
+    ?>
     
 </body>
 </html>
